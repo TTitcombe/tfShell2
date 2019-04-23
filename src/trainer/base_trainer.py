484ac6dc881e0c80ded2aca121fd67c829c1d6f5
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-23 19:31:06 +0100

    Fix flake8 errors

--- src/trainer/base_trainer.py
+++ src/trainer/base_trainer.py
@@ -6,59 +6,75 @@ import tensorflow as tf
 class BaseTrainer:
     """
     This class forms a base for training Tensorflow 2.0 models.
-    The basic training structure, looping through epochs and steps within epochs, has been implemented here.
-    Trainer classes should inherit from BaseTrainer and implement model-specific details, such as loss function.
+    The basic training structure, looping through epochs and steps within
+    epochs, has been implemented here.
+    Trainer classes should inherit from BaseTrainer and implement
+    model-specific details, such as loss function.
     """
     def __init__(self, model, batch_size=-1):
         """
         Initialise a BaseTrainer
         :param model: A Tensorflow 2.0 model to train
-        :param batch_size: optional int. The size of batches into which data should be split.
-                           A negative batch size denotes taking the data in one batch.
+        :param batch_size: optional int. The size of batches into which data
+                           should be split. A negative batch size denotes
+                           taking the data in one batch.
         """
         self.model = model
         self._batch_size = batch_size
 
-    def train(self, x_train, y_train, x_val=None, y_val=None, verbose=False, report_every=1):
+    def train(self, x_train, y_train, x_val=None, y_val=None,
+              verbose=False, report_every=1):
         """
         Train the model on provided data
-        :param x_train: numpy array containing training data. N x D where N is number of training examples
+        :param x_train: numpy array containing training data.
+                        N x D where N is number of training examples
         :param y_train: numpy array containing training labels. N x D_y
-        :param x_val: optional. numpy array containing validation data. N_val x D
-        :param y_val: optional. numpy array containing validation labels. N_val x D_y
-        :param report_every: optional int. How many epochs should be run between reporting of performance
-        :param verbose: optional bool. If true, report results from every training step during report epochs
-                        Default false
-        :return: final training score and validation score (None if x_val not provided)
+        :param x_val: optional. numpy array containing validation data.
+                      N_val x D
+        :param y_val: optional. numpy array containing validation labels.
+                      N_val x D_y
+        :param report_every: optional int. How many epochs should be run
+                             between reporting of performance
+        :param verbose: optional bool. If true, report results from every
+                        training step during report epochs. Default false.
+        :return: final training score and validation score
+                 (None if x_val not provided)
         """
         ################
         # Validate input
         ################
 
-        # report_every should not be negative. Quietly set this to every epoch if it is,
-        # as this would not be disastrous to training if not intended by user
+        # report_every should not be negative. Quietly set this to every
+        # epoch if it is, as this would not be disastrous to training
+        # if not intended by user
         report_every = max(1, report_every)
 
         # Check x and y train are compatible
         if x_train.shape[0] != y_train.shape[0]:
-            raise RuntimeError("BaseTrainer.train: x_train and y_train have a different number of data points. "
-                               "{} and {}, respectively.".format(x_train.shape[0], y_train.shape[0]))
+            raise RuntimeError("BaseTrainer.train: x_train and y_train have "
+                               "a different number of data points. {} and "
+                               "{}, respectively.".format(x_train.shape[0],
+                                                          y_train.shape[0]))
 
         # Check we have x and y validation data
-        if (x_val is None and y_val is not None) or (x_val is not None and y_val is None):
-            raise RuntimeError("BaseTrainer.train: You must either provide both "
-                               "validation data and labels, or neither.")
+        if (x_val is None and y_val is not None) or \
+                (x_val is not None and y_val is None):
+            raise RuntimeError("BaseTrainer.train: You must either provide "
+                               "both validation data and labels, or neither.")
 
         #####################
         # Loop through epochs
         #####################
         for epoch in range(self.n_epochs):
-            training_loss, training_score = self._train_epoch(x_train, y_train, verbose)
+            training_loss, \
+                training_score = self._train_epoch(x_train, y_train, verbose)
 
             if epoch % report_every == 0:
-                # TODO give users option to print, report to Tensorboard, or both.
+                # TODO give users option to print, report to Tensorboard,
+                #  or both.
                 print("\nEpoch {}...\nTraining Loss: {}; "
-                      "Training Score: {}\n".format(epoch, training_loss, training_score))
+                      "Training Score: {}\n".format(epoch, training_loss,
+                                                    training_score))
 
                 if x_val is not None:
                     val_score = self.validate(x_val, y_val)
@@ -70,7 +86,8 @@ class BaseTrainer:
             # See if training should exit early
             if self._exit_training(training_loss):
                 # TODO add logic for saving model
-                print("Exit condition met. Stopping training early at epoch {}".format(epoch))
+                print("Exit condition met. Stopping training early at "
+                      "epoch {}".format(epoch))
                 break
 
         return training_loss, training_score, val_score
@@ -98,13 +115,15 @@ class BaseTrainer:
 
             if verbose:
                 print("Step {}/{}...\n"
-                      "Score: {}; Loss: {}\n".format(step, n_steps, step_score, step_loss))
+                      "Score: {}; Loss: {}\n".format(step, n_steps, step_score,
+                                                     step_loss))
 
         return step_loss, step_score
 
     def _train_step(self, x_batch, y_batch):
         """
-        This method gets output from the model, calculates loss and score, and updates model variables
+        This method gets output from the model, calculates loss and score,
+        and updates model variables
         :param x_batch: numpy array of training data for the step
         :param y_batch: numpy array of training labels for the step
         :return: loss, score
@@ -113,39 +132,44 @@ class BaseTrainer:
             y_model = self.model(x_batch)
             step_loss = self._loss(y_model, y_batch)
         gradients = tape.gradient(step_loss, self.model.trainable_variables)
-        self.opimiser.apply_gradients(zip(gradients, self.model.trainable_variables))
+        self.opimiser.apply_gradients(zip(gradients,
+                                          self.model.trainable_variables))
 
         step_score = self.validate(y_model, y_batch)
         return step_loss, step_score
 
     def _loss(self, y_model, y):
         """
-        Calculate the loss for the model. This method must be implemented in a class
+        Calculate the loss for the model.
+        This method must be implemented in a class
         which inherits BaseTrainer.
         :param y_model: output from the model
         :param y: actual target data
         :return: loss, a float
         """
-        raise NotImplementedError("BaseTrainer._loss: _loss must be implemented in "
+        raise NotImplementedError("BaseTrainer._loss: _loss must be "
+                                  "implemented in "
                                   "a class which inherits BaseTrainer.")
 
     def validate(self, y_model, y):
         """
-        Calculate the score for model output. This method must be implemented in a class
+        Calculate the score for model output.
+        This method must be implemented in a class
         which inherits BaseTrainer.
         :param y_model: output from the model
         :param y: actual target data
         :return: score, a float
         """
-        raise NotImplementedError("BaseTrainer.validate: validate must be implemented in "
+        raise NotImplementedError("BaseTrainer.validate: validate must be "
+                                  "implemented in "
                                   "a class which inherits BaseTrainer.")
 
     def _exit_training(self, train_loss):
         """
         Evaluate if the training process should be exited early. This method
         contains a basic check for the loss being NaN.
-        In general, this method should be overwritten by an inheriting class for more
-        expressive training.
+        In general, this method should be overwritten by an
+        inheriting class for more expressive training.
         :param train_loss: The loss from the most recent epoch of training
         :return: a bool. If True, then we should exit training early
         """
@@ -160,9 +184,8 @@ class BaseTrainer:
         try:
             _batch_size = int(_batch_size)
         except ValueError:
-            raise ValueError("BaseTrainer.batch_size: batch size must be a positive integer. "
-                             "Provided batch size {} was type {}.".format(_batch_size, type(_batch_size)))
+            raise ValueError("BaseTrainer.batch_size: batch size must be a "
+                             "positive integer. Provided batch size {} was "
+                             "type {}.".format(_batch_size, type(_batch_size)))
 
         self._batch_size = _batch_size
-
-
--- test/trainer/test_base_trainer.py
+++ test/trainer/test_base_trainer.py
@@ -16,7 +16,8 @@ class BaseTrainerTest(unittest.TestCase):
         except NotImplementedError:
             pass
         else:
-            self.fail("_loss should not be implemented in the BaseTrainer class")
+            self.fail("_loss should not be implemented in the "
+                      "BaseTrainer class")
 
     def test_that_validate_raises_error(self):
         model = mock.Mock()
@@ -27,7 +28,8 @@ class BaseTrainerTest(unittest.TestCase):
         except NotImplementedError:
             pass
         else:
-            self.fail("_loss should not be implemented in the BaseTrainer class")
+            self.fail("_loss should not be implemented in the "
+                      "BaseTrainer class")
 
     def test_batch_size_property_must_be_int_like(self):
         model = mock.Mock()
@@ -38,7 +40,8 @@ class BaseTrainerTest(unittest.TestCase):
         except ValueError:
             pass
         else:
-            self.fail("batch_size should not be able to accept a string of text")
+            self.fail("batch_size should not be able to accept a "
+                      "string of text")
 
         try:
             trainer.batch_size = 128.0
@@ -59,9 +62,10 @@ class BaseTrainerTest(unittest.TestCase):
         trainer._train_epoch(x_data, y_data, False)
 
         expected_calls = [[x_data, y_data]]
-        self._assert_args_correct(trainer._train_step.call_args_list, expected_calls, "_train_step")
+        self._assert_args_correct(trainer._train_step.call_args_list,
+                                  expected_calls, "_train_step")
 
-    def test_that_number_of_training_steps_correctly_calculated_from_batch_size(self):
+    def test_that_number_of_training_steps_correctly_calculated_from_batch_size(self):  # noqa: E501
         model = mock.Mock()
         trainer = BaseTrainer(model)
         trainer._train_step = mock.MagicMock(return_value=(1.0, 1.0))
@@ -80,28 +84,34 @@ class BaseTrainerTest(unittest.TestCase):
         x_data_1, y_data_1 = x_data[:8], y_data[:8]
         x_data_2, y_data_2 = x_data[8:], y_data[8:]
         expected_calls = [[x_data_1, y_data_1], [x_data_2, y_data_2]]
-        self._assert_args_correct(trainer._train_step.call_args_list, expected_calls, "_train_step")
+        self._assert_args_correct(trainer._train_step.call_args_list,
+                                  expected_calls, "_train_step")
 
         # Test batch size = 1
         # With batch size 1 and n = 10, we should have 10 batches
-        trainer._train_step = mock.MagicMock(return_value=(1.0, 1.0))  # reset the call count
+        trainer._train_step = mock.MagicMock(return_value=(1.0, 1.0))
         trainer.batch_size = 1
 
         trainer._train_epoch(x_data, y_data, False)
 
         expected_calls = [[x_data[i:i+1], y_data[i:i+1]] for i in range(10)]
-        self._assert_args_correct(trainer._train_step.call_args_list, expected_calls, "_train_step")
+        self._assert_args_correct(trainer._train_step.call_args_list,
+                                  expected_calls, "_train_step")
 
-    def _assert_args_correct(self, mock_call_args_list, expected_calls, mock_object_name):
+    def _assert_args_correct(self, mock_call_args_list, expected_calls,
+                             mock_object_name):
         self.assertEqual(len(mock_call_args_list), len(expected_calls),
                          "{} should have been called {} times. It was called "
-                         "{} times instead.".format(mock_object_name, len(expected_calls), len(mock_call_args_list)))
+                         "{} times instead.".format(mock_object_name,
+                                                    len(expected_calls),
+                                                    len(mock_call_args_list)))
 
         for call, expected_args in zip(mock_call_args_list, expected_calls):
             actual_args, _ = call
             for actual_arg, expected_arg in zip(actual_args, expected_args):
                 self.assertTrue(np.array_equal(actual_arg, expected_arg),
-                                "{} does not equal {}".format(actual_arg, expected_arg))
+                                "{} does not equal {}".format(actual_arg,
+                                                              expected_arg))
 
 
 if __name__ == "__main__":

1066b07534944ca57de2f3b1dc7cabe3f6bd0d3d
Tom Titcombe <t.j.titcombe@gmail.com>
2019-04-24 10:07:51 +0100

    Create basic regression trainer example

--- src/trainer/basic_regression_trainer.py
+++ src/trainer/basic_regression_trainer.py
@@ -12,17 +12,16 @@ class BasicRegressionTrainer(BaseTrainer):
     This trainer works with basic regression models.
     """
     def __init__(self, model, batch_size=-1):
-        super().__init__(model, batch_size)
+        super(BasicRegressionTrainer, self).__init__(model, batch_size)
 
         # If there are any implementation-specific parameters,
         # add logic for them here
         self.mse = tf.keras.losses.MeanSquaredError()
 
-    @tf.function
     def _loss(self, y_model, y):
         return self.mse(y_model, y)
 
-    @tf.function
     def validate(self, y_model, y):
-        # In this basic example, score == loss
-        return self._loss(y_model, y)
+        m = tf.keras.metrics.MeanSquaredError()
+        m.update_state(y_model, y)
+        return m.result()
